20b5e9b1a85891b7ff9d8579261a98e7fd8230f9
Valentín Forbes <valenforbes@gmail.com>
2023-06-04 23:13:14 -0300

    Productos_App Primer commit

---
+++ productos_app/lib/screens/product_screen.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:productos_app/providers/product_form_provider.dart';
+import 'package:productos_app/services/services.dart';
+import 'package:productos_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../ui/input_decorations.dart';
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final productService = Provider.of<ProductsService>(context);
+
+    return ChangeNotifierProvider(
+        create: (_) => ProductFormProvider(productService.selectedProduct),
+        child: _ProductScreenBody(productService: productService));
+  }
+}
+
+class _ProductScreenBody extends StatelessWidget {
+  const _ProductScreenBody({
+    required this.productService,
+  });
+
+  final ProductsService productService;
+
+  @override
+  Widget build(BuildContext context) {
+    final productform = Provider.of<ProductFormProvider>(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        //como para q pueda ver el usuario lño q esta escribiendo, por eso lo comento
+        //keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ProductImage(url: productService.selectedProduct.picture),
+                Positioned(
+                    top: 60,
+                    left: 20,
+                    child: IconButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        icon: const Icon(
+                          Icons.arrow_back_ios_new,
+                          size: 40,
+                          color: Colors.white,
+                        ))),
+                Positioned(
+                    top: 60,
+                    right: 20,
+                    child: IconButton(
+                        onPressed: () async {
+                          final picker = ImagePicker();
+                          final XFile? pickedFile = await picker.pickImage(
+                          source: ImageSource.camera, imageQuality: 100);
+                          if (pickedFile == null) {
+
+                          return;
+                          }
+
+                          productService.updateSelectedProductImage(pickedFile.path);
+                        },
+                        icon: const Icon(
+                          Icons.camera_alt_outlined,
+                          size: 40,
+                          color: Colors.white,
+                        )))
+              ],
+            ),
+            const _ProductForm(),
+            const SizedBox(height: 100)
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+          onPressed: productService.isSaving ? null : () async {
+            if (!productform.isValidForm()) return;
+            final String? imageUrl = await productService.upLoadImage();
+            if (imageUrl != null) productform.product.picture = imageUrl;
+            await productService.saveOrCreateProduct(productform.product);
+          },
+          child: productService.isSaving ? const CircularProgressIndicator( color:  Colors.white,): const Icon(Icons.save_alt_outlined)),
+    );
+  }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm();
+
+  @override
+  Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+            key: productForm.formKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                TextFormField(
+                  initialValue: product.name,
+                  onChanged: (value) => product.name = value,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'El nombre es obligatorio';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecorations.authInputDecoration(
+                      hintText: 'Nombre del producto', labelText: 'Nombre'),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  initialValue: '${product.price}',
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(
+                        RegExp(r'^(\d+)?\.?\d{0,2}'))
+                  ],
+                  onChanged: (value) => {
+                    if (double.tryParse(value) == null)
+                      {product.price = 0}
+                    else
+                      {product.price = double.parse(value)}
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecorations.authInputDecoration(
+                      hintText: '\$150', labelText: 'Precio:'),
+                ),
+                const SizedBox(height: 30),
+                SwitchListTile(
+                    value: product.available,
+                    title: const Text('Disponible'),
+                    activeColor: Colors.indigo,
+                    onChanged: productForm.updateAvailability),
+                const SizedBox(height: 30),
+              ],
+            )),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                offset: const Offset(0, 5),
+                blurRadius: 5)
+          ]);
+}
---
+++ productos_app/lib/services/products_service.dart
@@ -0,0 +1,109 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:productos_app/models/models.dart';
+import 'package:http/http.dart' as http;
+
+class ProductsService extends ChangeNotifier {
+  final String _baseUrl = 'flutter-varios-d8c2d-default-rtdb.firebaseio.com';
+
+  final List<Product> products = [];
+
+  late Product selectedProduct;
+
+  File? newPictureFile;
+
+  bool isLoading = true;
+  bool isSaving = false;
+
+  ProductsService() {
+    loadProducts();
+  }
+
+  Future<List<Product>> loadProducts() async {
+    isLoading = true;
+    notifyListeners();
+
+    final url = Uri.https(_baseUrl, 'products.json');
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
+
+    productsMap.forEach((key, value) {
+      final tempProduct = Product.fromMap(value);
+      tempProduct.id = key;
+      products.add(tempProduct);
+    });
+
+    isLoading = false;
+    notifyListeners();
+
+    return products;
+  }
+
+  Future saveOrCreateProduct(Product product) async {
+    isSaving = true;
+    notifyListeners();
+    if (product.id == null) {
+      //creamos
+      await createProduct(product);
+    } else {
+      //actualizamos
+      await upDateProduct(product);
+    }
+    isSaving = false;
+    notifyListeners();
+  }
+
+  Future<String> upDateProduct(Product product) async {
+    final url = Uri.https(_baseUrl, 'products/${product.id}.json');
+    final resp = await http.put(url, body: product.toJson());
+    // ignore: unused_local_variable
+    final decodedData = resp.body;
+
+
+    final index = products.indexWhere((element) => element.id == product.id);
+    products[index] = product;
+    return product.id!;
+  }
+
+  Future<String> createProduct(Product product) async {
+    final url = Uri.https(_baseUrl, 'products.json');
+    final resp = await http.post(url, body: product.toJson());
+    final decodedData = json.decode(resp.body);
+    product.id = decodedData['name'];
+    products.add(product);
+
+    return product.id!;
+  }
+
+  void updateSelectedProductImage(String path) {
+    selectedProduct.picture = path;
+    newPictureFile = File.fromUri(Uri(path: path));
+    notifyListeners();
+  }
+
+  Future<String?> upLoadImage() async {
+    if (newPictureFile == null) return null;
+    isSaving = true;
+    notifyListeners();
+    final url = Uri.parse(
+        'https://api.cloudinary.com/v1_1/dogtdrc9e/image/upload?upload_preset=xzn6wlof');
+    final imageUploadRequest = http.MultipartRequest('POST', url);
+    final file =
+        await http.MultipartFile.fromPath('file', newPictureFile!.path);
+    imageUploadRequest.files.add(file);
+
+    final streamResponse = await imageUploadRequest.send();
+    final resp = await http.Response.fromStream(streamResponse);
+
+    if (resp.statusCode != 200 && resp.statusCode != 201) {
+     
+      return null;
+    }
+    newPictureFile = null;
+    final decodedData = json.decode(resp.body);
+    return decodedData['secure_url'];
+  }
+}
